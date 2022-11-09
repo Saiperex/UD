3ef598b79d34a02fd0b190288cb4429c6c51a893
Xavier <xaviramos668@gmail.com>
2022-11-09 16:58:12 -0300

    Landing

---
+++ script/script-index.js
@@ -0,0 +1,20 @@
+var imagenes=["bg-bailarina.png","bg-bailarina1.png"]
+var fondo=document.getElementById("img-animation");
+var index=0;
+function cambiarFondo()
+{
+    
+    if(index<1)
+    {
+        index=index+1;
+    }
+    else
+    {
+        index=0;
+    }
+    var url="imagenes/recursos/"+imagenes[index];
+    fondo.src=url;
+    fondo.style.transition="1s all";
+    tiempo=window.setTimeout('cambiarFondo()',5000)
+}
+cambiarFondo();
