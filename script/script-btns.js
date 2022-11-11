13bf22f926267b7130b48e4dc5941a83f35a40db
Xavier <xaviramos668@gmail.com>
2022-11-11 16:58:29 -0300

    Cabecera

---
+++ script/script-btns.js
@@ -0,0 +1,8 @@
+/*boton que va a inicio*/
+const btnVermas=document.getElementById("vermas");
+function ver()
+{
+    location.href="inicio.html";
+}
+btnVermas.onclick=function(){ver()};
+/*----------------- */
